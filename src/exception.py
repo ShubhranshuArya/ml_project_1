e2d1d8484108d8879b40e4bdc52947f41acb342e
ShubhranshuArya <developershubh69@gmail.com>
2024-08-14 09:38:30 +0100

    Data transformation completed.

--- src/exception.py
+++ src/exception.py
@@ -2,7 +2,9 @@ import sys
 import logging
 
 # Design of message for the exception
-def error_message_detail(error, error_detail:sys):
+
+
+def error_message_detail(error, error_detail: sys):
     _, _, exc_tb = error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
     error_message = "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
@@ -11,12 +13,13 @@ def error_message_detail(error, error_detail:sys):
     return error_message
 
 # Class which will be called to execute an exception.
+
+
 class CustomException(Exception):
-    def __init__(self, error_message, error_detail:sys):
+    def __init__(self, error_message, error_detail: sys):
         super().__init__(error_message)
-        self.error_message = error_message_detail(error_message, error_detail=error_detail)
+        self.error_message = error_message_detail(
+            error_message, error_detail=error_detail)
 
     def __str__(self):
         return self.error_message
-
-
